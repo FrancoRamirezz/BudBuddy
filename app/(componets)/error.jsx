af26e3a6e483a74597cdf5bc86ec9c80ad601667
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-11-01 18:06:45 -0700

    Add files via upload

---
+++ app/(componets)/error.jsx
@@ -0,0 +1,13 @@
+"use client"
+export default function Error({error,reset}){
+
+return(
+    <div>
+    <button onClick={() =>reset()}></button>  
+</div>
+
+)
+
+
+
+}
---
+++ app/(componets)/page.tsx
@@ -0,0 +1,18 @@
+
+import {useState} from "react";
+function Homepage(){
+
+
+return(
+
+<div>
+
+<h1 className="">
+Hello welcome to the shop
+</h1>
+
+
+</div>
+)
+};
+export default Homepage;
