ecb086271132b8a2b85b5349b839dd77ca54cc03
Francisco Ramirez <96508706+FrancoRamirezz@users.noreply.github.com>
2023-11-18 01:34:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from fastapi import FastAPI
-import AI.py
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, FileResponse
+import stripe
+import os
+# this will be used to get the ai component of the
+# os.system("python AI.py")
 app = FastAPI()
-@route("/")
-def new_route():
+
+origins = ['http://localhost:3000']
+app.add_middleware(
+
+    CORSMiddleware,
+    allow_orgins = origins,
+    allow_credentials = True,
+    allow_methods  = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.get('/')
+async def route():
     return {"App": "Hello Virgo AI Saas"}
-@route("/Post")
-def openai_function():
-    pass
-# will add the new functions below this 
-# to ensure that the connection is used we must use Crossware
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host = "")
+
+
+@app.get('/favicon.ico')
+def icon_getter():
+    return FileResponse("public/favicon.ico", media_type="image/x-icon" )
+
 
 
