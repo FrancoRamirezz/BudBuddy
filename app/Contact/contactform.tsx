693a2e3f441dad1fa425d7c2a19a85e370a57184
Franco <96508706+FrancoRamirezz@users.noreply.github.com>
2024-01-22 15:14:34 -0800

    Update contactform.tsx

--- app/Contact/contactform.tsx
+++ app/Contact/contactform.tsx
@@ -1,18 +1,38 @@
-export default function ContactForm(){
+'use client'
+import { useState } from "react";
 
+function contact(){
 
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [message, setMessage] = useState('');
 
+const onSumbit= () =>{
 
-
-
+}
 return(
+<div>
+<h1>Contact Us </h1>
 
-<></>
+<p>Glad that you can join us! If you need have any questions, or want to connect. Then you can reach out by email address </p>
+<form>
+
+<input value = {name} type = 'text' placeholder = "Name"
+onClick={e =>name }/>
+
+<input value = {email} type = "text" placeholder = "Email"/>
+<input value = {message} type = 'text' placeholder="Message"/>
 
 
 
+</form>
+
+
+
+</div>
 )
 };
+export default contact;
 
 
 
