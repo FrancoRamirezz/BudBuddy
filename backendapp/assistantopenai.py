e46a4cf1b778de33e35103244ace5dd5cabe04f0
Franco <96508706+FrancoRamirezz@users.noreply.github.com>
2024-01-23 16:02:20 -0800

    Update assistantopenai.py

--- backendapp/assistantopenai.py
+++ backendapp/assistantopenai.py
@@ -4,7 +4,7 @@ import os
 OPENAI_API_KEY= "<YOUR PRIVATE KEY>"
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 client = openai.OpenAI()
-
+model = "gpt-3.5-turbo-16k"
 
 # the upload file function part 
 def file_upload(file):
