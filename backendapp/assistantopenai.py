630c09e8a39b840a556959928f88b6c4b2463661
Franco <96508706+FrancoRamirezz@users.noreply.github.com>
2024-01-24 19:38:22 -0800

    Update assistantopenai.py

--- backendapp/assistantopenai.py
+++ backendapp/assistantopenai.py
@@ -7,5 +7,5 @@ client = openai.OpenAI()
 model = "gpt-3.5-turbo-16k"
 
 # the upload file function part 
-def file_upload(file):
-    file_path = open
+def client_assistant(client):
+    cannabis_ass = client.beta
